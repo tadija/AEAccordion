2ffa80bc078088e307dfcb64d19fcbf78ee35e56
Marko Tadić <tadija@me.com>
2017-06-19 23:59:35 +0200

    Added initial version of Package.swift and CHANGELOG.md Removed license from source code (repo license is enough) Minor other changes

---
+++ CHANGELOG.md
@@ -0,0 +1,21 @@
+# Changelog
+
+## Version 2.0.0
+
+- 
+
+## Version 1.0.3
+
+- Minor changes
+
+## Version 1.0.2
+
+- Minor changes
+
+## Version 1.0.1
+
+- Minor changes
+
+## Version 1.0.0
+
+- Initial version
--- Sources/AEAccordion.h
+++ Sources/AEAccordion.h
@@ -1,19 +1,4 @@
-//
-//  AEAccordion.h
-//  AEAccordion
-//
-//  Created by Marko Tadić on 6/19/17.
-//  Copyright © 2017 AE. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
-//! Project version number for AEAccordion.
 FOUNDATION_EXPORT double AEAccordionVersionNumber;
-
-//! Project version string for AEAccordion.
 FOUNDATION_EXPORT const unsigned char AEAccordionVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <AEAccordion/PublicHeader.h>
-
-
